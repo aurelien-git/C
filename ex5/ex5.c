53070a39046ba10e1fd48b14cfe2705bb910237f
Aurélien DESBRIÈRES <aurelien.desbrieres@gmail.com>
2016-01-31 09:59:16 +0100

    ex5 directory

---
+++ ex5/ex5.c
@@ -0,0 +1,17 @@
+/*
+ * Learn C the hard way
+ */
+
+#include <stdio.h>
+
+/* Comment of the files */
+int main(int argc, char *argv[])
+{
+  int distance = 100;
+
+  // Different type of comment
+  printf("You are %d miles away.\n", distance);
+
+  return 0;
+}
+
