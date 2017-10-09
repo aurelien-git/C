024972ff42d71e8403d1501a2c48c1531504bbef
Aurélien DESBRIÈRES <adesbrieres@mail.kbn>
2017-10-09 10:59:47 +0200

    deeper in stdio

--- x0001/a.c
+++ x0001/a.c
@@ -10,7 +10,7 @@ Write with Emacs-Nox ───────────────────�
 C - a ─────────────────────────────────────────┘
 */
 
-#include <stdio.h>                  // man stdio I/O library functions
+#include <stdio.h>                      // man stdio I/O library functions
 
 int total = 0;
 int sum (int, int);
@@ -18,17 +18,33 @@ int sum (int, int);
 // Main function
 int main(int argc, char *argv[])
 {
-    printf("Fucking Hell!\n");
+    printf("Fucking Hell!\n");          // Because it must be said!
 
-    total = sum (1, 1);
+    // How to print different type of characters, strings, and values
+    char ch = 'A';
+    char str[42] = "One day no one will make me piss anymore!";
+    float flt = 10.234;
+    int no = 150;
+    double dbl = 20.123456;
+
+    printf("Character is %c \n", ch);
+    printf("\n\t\t\t%s \n\n", str);
+    printf("Float value is %f \n", flt);
+    printf("Integer value is %d \n", no);
+    printf("Double value is %lf \n", dbl);
+    printf("Octal value is %o\n", no);
+    printf("Hexadecimal value is %x \n", no);
+
+    total = sum (1, 1);                 // Calculus made from defined function I
     printf("Total is: %d\n", total);
 
     printf("Hit Enter to exit\n");
-    getchar();                      // Stand from keyboard
+    getchar();                          // Stand from keyboard
 
     return 0;
 }
 
+// Defined function I
 int sum (int a, int b)
 {
     return a + b;
