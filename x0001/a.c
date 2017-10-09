99d579cbf95c8738c4f16f7ae5a258eb33eb9589
Aurélien DESBRIÈRES <adesbrieres@mail.kbn>
2017-10-09 14:05:42 +0200

    deeper in stdio

--- x0001/a.c
+++ x0001/a.c
@@ -10,7 +10,7 @@ Write with Emacs-Nox ───────────────────�
 C - a ─────────────────────────────────────────┘
 */
 
-#include <stdio.h>                      // man stdio I/O library functions
+#include <stdio.h>                                          // man stdio I/O library functions
 
 int total = 0;
 int sum (int, int);
@@ -18,11 +18,11 @@ int sum (int, int);
 // Main function
 int main(int argc, char *argv[])
 {
-    printf("Fucking Hell!\n");          // Because it must be said!
+    printf("Fucking Hell!\n");                              // Because it must be said!
 
     // How to print different type of characters, strings, and values
     char ch = 'A';
-    char str[42] = "One day no one will make me piss anymore!";
+    char str[42] = "One day no one will make me piss anymore!";  // Just the perfect primer char sentence
     float flt = 10.234;
     int no = 150;
     double dbl = 20.123456;
@@ -35,14 +35,25 @@ int main(int argc, char *argv[])
     printf("Octal value is %o\n", no);
     printf("Hexadecimal value is %x \n", no);
 
-    total = sum (1, 1);                 // Calculus made from defined function I
+    total = sum (1, 1);                                     // Calculus made from defined function I
     printf("Total is: %d\n", total);
 
+    char ICE_CUBE;                                          // Scanf to read characters from keyboard
+    printf("Enter any character \n");
+    scanf("%c", &ICE_CUBE);                                 // &ch for consecutive characters &str for string
+    printf("So: %c \nIs what you mean \n", ICE_CUBE);
+
+    char HAIL_STONE[42];                                    // Scanf > 42
+    printf("Enter any string ( upto 42 characters ) \n");
+    scanf("%s", HAIL_STONE);                                // &str for string
+    printf("So: %s \nIs what you mean \n", HAIL_STONE);
+
     printf("Hit Enter to exit\n");
-    getchar();                          // Stand from keyboard
+    getchar();                                              // Stand from keyboard
 
     return 0;
 }
+
 
 // Defined function I
 int sum (int a, int b)
