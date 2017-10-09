9261cda7337b964552e33982bbea3f343296ac96
Aurélien DESBRIÈRES <adesbrieres@mail.kbn>
2017-10-09 10:25:13 +0200

    C Exp in I/O

---
+++ x0001/a.c
@@ -0,0 +1,22 @@
+/*
+C - Stack of C Exp
+License    : GNU GPL v3 or later
+Author     : Aurélien DESBRIERES
+Mail       : aurelien@hackers.camp
+Project    : Programming C Exp
+Created on : Monday October 9 2017
+
+Write with Emacs-Nox ──────────────────────────┐
+C - a ─────────────────────────────────────────┘
+*/
+
+#include <stdio.h>                  // man stdio I/O library functions
+
+// Main function
+int main(int argc, char *argv[])
+{
+    printf("Fucking Hell!\n");
+    getchar();                      // Stand from keyboard
+
+    return 0;
+}
